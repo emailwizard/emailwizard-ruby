85d0d6659621ff72b1d8ab91b476924f9f83c3eb
Denys Potapov <denys.potapov@gmail.com>
2018-10-22 16:56:47 +0300

    add config

---
+++ lib/email_wizard/config.rb
@@ -0,0 +1,8 @@
+class EmailWizard::Config
+  def initialize(api_key, provider, provider_credentials, from)
+    @api_key = api_key
+    @provider = provider
+    @provider_credentials = provider_credentials
+    @from = from
+  end
+end
