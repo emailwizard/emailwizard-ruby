8b40d80c97c0bb133926accc9e39c2c1916fa014
Denys Potapov <denys.potapov@gmail.com>
2018-11-06 15:24:56 +0200

    add default project id

--- lib/email_wizard/client.rb
+++ lib/email_wizard/client.rb
@@ -1,7 +1,33 @@
 class EmailWizard::Client
   @@config = nil
 
+  @@current_project_id = nil
+
+  def self.config=(config)
+  	@@config = config
+  end
+
+  def self.current_project_id=(project_id)
+  	@@current_project_id = project_id
+  end
+
   def self.configured?
     raise EmailWizard::NoConfigError if @@config.nil?
   end
+
+  def self.send_template()
+  end
+
+  def self.fetch_template(project_id: nil, template_name:, payload:)
+  	response = post(project_id, :fetch, payload)
+  end
+
+  private
+
+  def self.post(project_id, action, data)
+  	configured?
+  	project_id = project_id || @@current_project_id
+  	raise EmailWizard::ProjectUnknownError if project_id.nil?
+  end
+
 end
--- spec/email_wizard/client_spec.rb
+++ spec/email_wizard/client_spec.rb
@@ -1,5 +1,30 @@
 RSpec.describe EmailWizard::Client do
+  def configured_client()
+  	EmailWizard::Client.config = EmailWizard::Config.new(
+      api_key: 1,
+      provider: :sendgrid,
+      provider_credentials: {
+        api_key: 2
+      },
+      from: 'no-reply@example.org'
+    )
+    EmailWizard::Client
+  end
+
   it "raises error if not cofigured" do
     expect{EmailWizard::Client.configured?}.to raise_error(EmailWizard::NoConfigError)
   end
+
+  it "uses default project id if note set" do
+    client = configured_client
+    client.current_project_id = 1
+    client.fetch_template(template_name: 'test', payload: {})
+  end
+
+  it "throws an error if no project id" do
+  	client = configured_client
+    client.current_project_id = nil
+    expect{client.fetch_template(template_name: 'test', payload: {})}.to raise_error(EmailWizard::ProjectUnknownError)
+  end
+
 end
