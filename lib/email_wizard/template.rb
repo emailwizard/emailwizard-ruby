91eebea37fc1f78a1230e8eb63778e685dfbbabc
Denys Potapov <denys.potapov@gmail.com>
2019-01-04 15:11:52 +0200

    add template

---
+++ lib/email_wizard/template.rb
@@ -0,0 +1,21 @@
+require 'base64'
+
+module EmailWizard
+  # Rendered template
+  class Template
+    attr_reader :html
+    attr_reader :text
+
+    def initialize(html:, text:)
+      @html = html
+      @text = text
+    end
+
+    def self.from_json(json)
+      Template.new(
+        html: Base64.decode64(json[:html]),
+        text: Base64.decode64(json[:text])
+      )
+    end
+  end
+end
