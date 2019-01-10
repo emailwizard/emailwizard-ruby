7975010254f94bf70d14d6a4b533ff156e780997
Denys Potapov <denys.potapov@gmail.com>
2019-01-10 05:06:19 +0200

    hash config

--- spec/email_wizard/config_spec.rb
+++ spec/email_wizard/config_spec.rb
@@ -1,12 +1,17 @@
 RSpec.describe EmailWizard::Config do
-  it 'inits' do
-    EmailWizard::Config.new(
+  it 'returns config hash' do
+    config = EmailWizard::Config.new(
       api_key: 1,
       provider: :sendgrid,
       provider_credentials: {
         api_key: 2
       },
       from: 'no-reply@example.org'
+    )
+
+    expect(config.as_hash).to eq(
+      from: 'no-reply@example.org',
+      'sendgrid_credentials' => { api_key: 2 }
     )
   end
 end
